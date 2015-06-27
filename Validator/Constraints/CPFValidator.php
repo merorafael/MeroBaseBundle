d1afb0f070c0c5fe7b42f8ee48686d628f2e4928
merorafael <merorafael@gmail.com>
2015-06-27 02:01:51 -0300

    Ajustes no validador de CPF usando expressão regular.

--- Validator/Constraints/CPFValidator.php
+++ Validator/Constraints/CPFValidator.php
@@ -19,38 +19,26 @@ class CPFValidator extends ConstraintValidator
             throw new UnexpectedTypeException($constraint, __NAMESPACE__ . "\CPF");
         }
         if (!empty($value)) {
+            if (!preg_match("^\d{3}.\d{3}.\d{3}-\d{2}$", $value)) {
+                $this->buildViolation($constraint->message)
+                    ->setParameter("{{ value }}", $this->formatValue($value))
+                    ->addViolation();
+            }
             $value = preg_replace("[^0-9]", "", $value);
-            $value = str_pad($value, 11, "0", STR_PAD_LEFT);
-            if (strlen($value) != 11) {
+            if (!preg_match("(?!(\d)\1{10})\d{11}", $value)) {
                 $this->buildViolation($constraint->message)
                     ->setParameter("{{ value }}", $this->formatValue($value))
                     ->addViolation();
-            } else if (
-                $value == "00000000000" ||
-                $value == "11111111111" ||
-                $value == "22222222222" ||
-                $value == "33333333333" ||
-                $value == "44444444444" ||
-                $value == "55555555555" ||
-                $value == "66666666666" ||
-                $value == "77777777777" ||
-                $value == "88888888888" ||
-                $value == "99999999999"
-            ) {
-                $this->buildViolation($constraint->message)
-                    ->setParameter("{{ value }}", $this->formatValue($value))
-                    ->addViolation();
-            } else {
-                for ($t = 9; $t < 11; $t++) {
-                    for ($d = 0, $c = 0; $c < $t; $c++) {
-                        $d += $value{$c} * (($t + 1) - $c);
-                    }
-                    $d = ((10 * $d) % 11) % 10;
-                    if ($value{$c} != $d) {
-                        $this->buildViolation($constraint->message)
-                            ->setParameter("{{ value }}", $this->formatValue($value))
-                            ->addViolation();
-                    }
+            }
+            for ($t = 9; $t < 11; $t++) {
+                for ($d = 0, $c = 0; $c < $t; $c++) {
+                    $d += $value{$c} * (($t + 1) - $c);
+                }
+                $d = ((10 * $d) % 11) % 10;
+                if ($value{$c} != $d) {
+                    $this->buildViolation($constraint->message)
+                        ->setParameter("{{ value }}", $this->formatValue($value))
+                        ->addViolation();
                 }
             }
         }
