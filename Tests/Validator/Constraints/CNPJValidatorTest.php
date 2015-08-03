cf074a8edbccc0fac99cba102c1013f525b404be
Rafael Mello <merorafael@gmail.com>
2015-08-03 10:05:50 -0300

    Added test case to CNPJValidator; CNPJValidator bugfix.

---
+++ Tests/Validator/Constraints/CNPJValidatorTest.php
@@ -0,0 +1,76 @@
+<?php
+namespace Mero\Bundle\BaseBundle\Tests\Validator\Constraints;
+
+use Mero\Bundle\BaseBundle\Validator\Constraints\CNPJ;
+use Mero\Bundle\BaseBundle\Validator\Constraints\CNPJValidator;
+use Symfony\Component\Validator\Tests\Constraints\AbstractConstraintValidatorTest;
+use Symfony\Component\Validator\Validation;
+
+class CNPJValidatorTest extends AbstractConstraintValidatorTest
+{
+
+    protected function getApiVersion()
+    {
+        return Validation::API_VERSION_2_5;
+    }
+
+    protected function createValidator()
+    {
+        return new CNPJValidator();
+    }
+
+    public function testNullIsValid()
+    {
+        $this->validator->validate(null, new CNPJ());
+        $this->assertNoViolation();
+    }
+
+    public function testEmptyStringIsValid()
+    {
+        $this->validator->validate('', new CNPJ());
+        $this->assertNoViolation();
+    }
+
+
+    public function getInvalidCNPJs()
+    {
+        return [
+            ['11.111.111/1111-11'],
+            ['22.222.222/2222-22'],
+            ['66.121.538/0001-00']
+        ];
+    }
+
+    /**
+     * @dataProvider getInvalidCNPJs
+     */
+    public function testInvalidCNPJs($cnpj)
+    {
+        $constraint = new CNPJ([
+            "message" => "testMessage"
+        ]);
+        $this->validator->validate($cnpj, $constraint);
+        $this->buildViolation('testMessage')
+            ->setParameter('{{ value }}', '"'.$cnpj.'"')
+            ->assertRaised();
+    }
+
+    public function getValidCNPJs()
+    {
+        return [
+            ['06.785.165/0001-00'],
+            ['66.121.538/0001-62'],
+            ['32.771.783/0001-01']
+        ];
+    }
+
+    /**
+     * @dataProvider getValidCNPJs
+     */
+    public function testValidCNPJs($cnpj)
+    {
+        $this->validator->validate($cnpj, new CNPJ());
+        $this->assertNoViolation();
+    }
+
+}
--- Validator/Constraints/CNPJValidator.php
+++ Validator/Constraints/CNPJValidator.php
@@ -19,47 +19,47 @@ class CNPJValidator extends ConstraintValidator
     public function validate($value, Constraint $constraint)
     {
         if (!$constraint instanceof CNPJ)
-            throw new UnexpectedTypeException($constraint, __NAMESPACE__.'\CNPJ');
-        if (!empty($value)) {
-            $value = preg_replace('/[^0-9]/', '', $value);
-            if (strlen($value) != 14) {
-                $this->buildViolation($constraint->message)
-                    ->setParameter('{{ value }}', $this->formatValue($value))
-                    ->addViolation();
-                return;
-            }
-            for ($i = 0, $aux = 5, $count = 0; $i < 12; $i++) {
-                $count += $value{$i}*$aux;
-                $aux = ($aux == 2)
-                    ? 9
-                    : $aux - 1;
-            }
-            $d1 = $count % 11;
-            $d1 = $d1 < 2
-                ? 0
-                : 11 - $d1;
-            if ($value{12} != $d1) {
-                $this->buildViolation($constraint->message)
-                    ->setParameter('{{ value }}', $this->formatValue($value))
-                    ->addViolation();
-                return;
-            }
-            for ($i = 0, $aux = 6, $count = 0; $i < 13; $i++) {
-                $count += $value{$i}*$aux;
-                $aux = ($aux == 2)
-                    ? 9
-                    : $aux - 1;
-            }
-            $d2 = $count % 11;
-            $d2 = $d2 < 2
-                ? 0
-                : 11 - $d2;
-            if ($value{13} != $d2) {
-                $this->buildViolation($constraint->message)
-                    ->setParameter('{{ value }}', $this->formatValue($value))
-                    ->addViolation();
-                return;
-            }
+            throw new UnexpectedTypeException($constraint, __NAMESPACE__ . '\CNPJ');
+        if (null === $value || '' === $value)
+            return;
+        $value_number = preg_replace('/[^0-9]/', '', $value);
+        if (strlen($value_number) != 14) {
+            $this->buildViolation($constraint->message)
+                ->setParameter('{{ value }}', $this->formatValue($value))
+                ->addViolation();
+            return;
+        }
+        for ($i = 0, $aux = 5, $count = 0; $i < 12; $i++) {
+            $count += $value_number{$i} * $aux;
+            $aux = ($aux == 2)
+                ? 9
+                : $aux - 1;
+        }
+        $d1 = $count % 11;
+        $d1 = $d1 < 2
+            ? 0
+            : 11 - $d1;
+        if ($value_number{12} != $d1) {
+            $this->buildViolation($constraint->message)
+                ->setParameter('{{ value }}', $this->formatValue($value))
+                ->addViolation();
+            return;
+        }
+        for ($i = 0, $aux = 6, $count = 0; $i < 13; $i++) {
+            $count += $value_number{$i} * $aux;
+            $aux = ($aux == 2)
+                ? 9
+                : $aux - 1;
+        }
+        $d2 = $count % 11;
+        $d2 = $d2 < 2
+            ? 0
+            : 11 - $d2;
+        if ($value_number{13} != $d2) {
+            $this->buildViolation($constraint->message)
+                ->setParameter('{{ value }}', $this->formatValue($value))
+                ->addViolation();
+            return;
         }
     }
 
