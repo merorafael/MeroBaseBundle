44cb562de93da1d9c5b017afe578213c37eb9e83
Rafael Mello <merorafael@gmail.com>
2015-08-03 08:22:23 -0300

    Fix validation algorithm in CNPJValidator; Validators optimization.

--- Validator/Constraints/CNPJValidator.php
+++ Validator/Constraints/CNPJValidator.php
@@ -19,14 +19,47 @@ class CNPJValidator extends ConstraintValidator
     public function validate($value, Constraint $constraint)
     {
         if (!$constraint instanceof CNPJ)
-            throw new UnexpectedTypeException($constraint, __NAMESPACE__ . "\BrazilianCNPJ");
+            throw new UnexpectedTypeException($constraint, __NAMESPACE__.'\CNPJ');
         if (!empty($value)) {
-            $value = preg_replace("/[^0-9]/", "", $value);
-            if (strlen($value) != 14)
+            $value = preg_replace('/[^0-9]/', '', $value);
+            if (strlen($value) != 14) {
                 $this->buildViolation($constraint->message)
-                    ->setParameter("{{ value }}", $this->formatValue($value))
+                    ->setParameter('{{ value }}', $this->formatValue($value))
                     ->addViolation();
-
+                return;
+            }
+            for ($i = 0, $aux = 5, $count = 0; $i < 12; $i++) {
+                $count += $value{$i}*$aux;
+                $aux = ($aux == 2)
+                    ? 9
+                    : $aux - 1;
+            }
+            $d1 = $count % 11;
+            $d1 = $d1 < 2
+                ? 0
+                : 11 - $d1;
+            if ($value{12} != $d1) {
+                $this->buildViolation($constraint->message)
+                    ->setParameter('{{ value }}', $this->formatValue($value))
+                    ->addViolation();
+                return;
+            }
+            for ($i = 0, $aux = 6, $count = 0; $i < 13; $i++) {
+                $count += $value{$i}*$aux;
+                $aux = ($aux == 2)
+                    ? 9
+                    : $aux - 1;
+            }
+            $d2 = $count % 11;
+            $d2 = $d2 < 2
+                ? 0
+                : 11 - $d2;
+            if ($value{13} != $d2) {
+                $this->buildViolation($constraint->message)
+                    ->setParameter('{{ value }}', $this->formatValue($value))
+                    ->addViolation();
+                return;
+            }
         }
     }
 
--- Validator/Constraints/CPFValidator.php
+++ Validator/Constraints/CPFValidator.php
@@ -19,21 +19,24 @@ class CPFValidator extends ConstraintValidator
     public function validate($value, Constraint $constraint)
     {
         if (!$constraint instanceof CPF)
-            throw new UnexpectedTypeException($constraint, __NAMESPACE__ . "\BrazilianCPF");
+            throw new UnexpectedTypeException($constraint, __NAMESPACE__.'\CPF');
         if (!empty($value)) {
-            $value = preg_replace("/[^0-9]/", "", $value);
-            if (strlen($value) != 11)
+            $value = preg_replace('/[^0-9]/', '', $value);
+            if (strlen($value) != 11) {
                 $this->buildViolation($constraint->message)
-                    ->setParameter("{{ value }}", $this->formatValue($value))
+                    ->setParameter('{{ value }}', $this->formatValue($value))
                     ->addViolation();
+                return;
+            }
             for ($t = 9; $t < 11; $t++) {
                 for ($d = 0, $c = 0; $c < $t; $c++)
                     $d += $value{$c} * (($t + 1) - $c);
                 $d = ((10 * $d) % 11) % 10;
                 if ($value{$c} != $d) {
                     $this->buildViolation($constraint->message)
-                        ->setParameter("{{ value }}", $this->formatValue($value))
+                        ->setParameter('{{ value }}', $this->formatValue($value))
                         ->addViolation();
+                    return;
                 }
             }
         }
