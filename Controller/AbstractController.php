02a090e9879037b7960b56ec648ad29dec9fcf55
Rafael Mello <merorafael@gmail.com>
2015-12-14 15:54:45 -0200

    Insight optimization

--- Controller/AbstractController.php
+++ Controller/AbstractController.php
@@ -8,9 +8,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Serializer\Encoder\JsonEncoder;
-use Symfony\Component\Serializer\Encoder\XmlEncoder;
-use Symfony\Component\Serializer\Serializer;
 
 class AbstractController extends Controller
 {
