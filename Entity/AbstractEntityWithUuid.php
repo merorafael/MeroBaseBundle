e9a902ac5739e06d43d726d1204430da27f2e433
merorafael <merorafael@gmail.com>
2016-02-29 14:58:22 -0300

    AbstractEntityWithUuid bugfix

--- Entity/AbstractEntityWithUuid.php
+++ Entity/AbstractEntityWithUuid.php
@@ -3,6 +3,14 @@ namespace Mero\Bundle\BaseBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 
+/**
+ * Common entity class.
+ *
+ * @author Rafael Mello <merorafael@gmail.com>
+ *
+ * @ORM\MappedSuperclass
+ * @ORM\HasLifecycleCallbacks
+ */
 abstract class AbstractEntityWithUuid
 {
     use Field\UuidTrait, Field\CreatedTrait, Field\ModifiedTrait;
